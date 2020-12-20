f2186fdf9f1894075abd154c8a445d646ec6cce8
iravikiran <iamravikirank@gmail.com>
2020-12-20 17:20:18 +0530

    Updated Sources for Calculator v1.0

--- README.md
+++ README.md
@@ -2,28 +2,32 @@
 GUI Calculator built with Py-tkinter. - Is a Cross platform Calculator application built with tkinter for GUI support on Python. 
 
 # Pre-Requsites.
-Python v3.8x+
-tkinter v3.8+ 
+1. Python v3.8x+
+2. tkinter v3.8+ 
 
 # Installation (Linux). 
 Installation for tkinter. 
-	- sudo apt-get install python3-tk 
+	sudo apt-get install python3-tk 
 
 Installation for Forwarding X11 - (optional, required for Remote Target.)
-	- sudo apt-get install x11-apps
+	sudo apt-get install x11-apps
 
 # Calculator Features.
 1. Addation
 2. Subtraction
 3. Multiplication
 4. Division.
+5. Power of Number
+6. Square.
+7. Percentage
 
 # Version.
-Calculator v0.2
+Calculator v1.0
 
 # TODO.
-1. Theme & Color button.
-2. Square Root.
-3. Power  
-4. Percentage
+1. More Scientific Functions.
+2. Light & Dark Color scheme. 
 
+# Author
+Insta-ID : ravikiran.me
+Web-URL	 : https://ravi-kiran.me
--- Sources/Calculator.py
+++ Sources/Calculator.py
@@ -1,5 +1,5 @@
 #
-#               PyGUI - Calculator v0.2
+#               Python-Tkinter - Calculator v1.0
 #
 #   A Simple Python based GUI Calculator Application written
 #   with Tkinter.
@@ -8,6 +8,9 @@
 #           ->   Subtraction of Numbers.
 #           ->   Multiplication of Numbers.
 #           ->   Division of Numbers.
+#           ->	 Power of a Number.
+#			->   Square of a Number.
+#			->   Percentage of a Number.
 #
 #   Feel free to add more features to the code and create a PR
 #   i'm working on adding more features on the next releases.
@@ -26,7 +29,7 @@ root = Tk()
 # Title of the Application.
 root.title("Calculator")
 
-root.geometry('395x305')
+root.geometry('395x370')
 
 # User Input field, 40px width set as default.
 e = Entry(root, width=45, borderwidth=5)
@@ -47,6 +50,27 @@ def button_clear():
     e.delete(0, END)
 
 
+# Checks for the Math operand and then performs the arthematic operation.
+def button_equal():
+	if math == "square":
+		e.insert(0, f_num * f_num)
+	elif math == "percent":
+		e.insert(0, f_num / 100)
+	else:
+		sec = e.get()
+		e.delete(0, END)
+		if math == "addition":
+		    e.insert(0, f_num + int(sec))
+		if math == "subtract":
+		    e.insert(0, f_num - int(sec))
+		if math == "multiply":
+		    e.insert(0, f_num * int(sec))
+		if math == "divide":
+		    e.insert(0, f_num / int(sec))
+		if math == "power":
+			e.insert(0, f_num ** int(sec))
+
+
 # Function to Add and capture the first number.
 def button_add():
     first = e.get()
@@ -57,21 +81,7 @@ def button_add():
     e.delete(0, END)
 
 
-# Checks for the Math operand and then performs the arthematic operation.
-def button_equal():
-    sec = e.get()
-    e.delete(0, END)
-    if math == "addition":
-        e.insert(0, f_num + int(sec))
-    if math == "subtract":
-        e.insert(0, f_num - int(sec))
-    if math == "multiply":
-        e.insert(0, f_num * int(sec))
-    if math == "divide":
-        e.insert(0, f_num / int(sec))
-
-
-# Function to Add and capture the first number.
+# Function to Sub and capture the first number.
 def button_sub():
     first = e.get()
     global f_num
@@ -81,7 +91,7 @@ def button_sub():
     e.delete(0, END)
 
 
-# Function to Add and capture the first number.
+# Function to Multiply and capture the first number.
 def button_mul():
     first = e.get()
     global f_num
@@ -91,12 +101,42 @@ def button_mul():
     e.delete(0, END)
 
 
-# Function to Add and capture the first number.
+# Function to Divide and capture the first number.
 def button_div():
     first = e.get()
     global f_num
     global math
     math = "divide"
+    f_num = int(first)
+    e.delete(0, END)
+
+
+# Function to Power and capture the first number.
+def button_pow():
+    first = e.get()
+    global f_num
+    global math
+    math = "power"
+    f_num = int(first)
+    e.delete(0, END)
+
+
+# Function to Squaring and capture the first number.
+def button_sqr():
+    first = e.get()
+    global f_num
+    global math
+    math = "square"
+    f_num = int(first)
+    e.delete(0, END)
+
+
+# Function to Percentage and capture the first number.
+def button_pst():
+    first = e.get()
+    global f_num
+    global math
+    math = "percent"
     f_num = int(first)
     e.delete(0, END)
 
@@ -113,13 +153,16 @@ button8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(
 button9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 
-# Buttons declaration for add, mul, sub, equal, div, and clear.
+# Buttons declaration for add, mul, sub, equal, div, clear, percent, power of, & square.
 button_add = Button(root, text="+", padx=39, pady=20, command=button_add, bg="#ff9e00", fg="#fbfbfb")
-button_equal = Button(root, text="=", padx=39, pady=20, command=button_equal, bg="#ffe95a")
+button_equal = Button(root, text="=", padx=39, pady=51, command=button_equal, bg="#ffe95a")
 button_clear = Button(root, text="C", padx=39, pady=20, command=button_clear, bg="#ff9e00", fg="#fbfbfb")
 button_sub = Button(root, text="—", padx=38, pady=20, command=button_sub, bg="#ff9e00", fg="#fbfbfb")
 button_mul = Button(root, text="X", padx=39, pady=20, command=button_mul, bg="#ff9e00", fg="#fbfbfb")
-button_div = Button(root, text="/", padx=40, pady=20, command=button_div, bg="#ff9e00", fg="#fbfbfb")
+button_div = Button(root, text="/", padx=42, pady=20, command=button_div, bg="#ff9e00", fg="#fbfbfb")
+button_pow = Button(root, text="Pow", padx=30, pady=20, command=button_pow, bg="#ff9e00", fg="#fbfbfb")
+button_sqr = Button(root, text="x^2", padx=31, pady=20, command=button_sqr, bg="#ff9e00", fg="#fbfbfb")
+button_pst = Button(root, text="%", padx=38, pady=20, command=button_pst, bg="#ff9e00", fg="#fbfbfb")
 
 # Packing up the buttons 1, 2, 3, & + on Row3.
 button1.grid(row=3, column=0)
@@ -139,10 +182,16 @@ button8.grid(row=1, column=1)
 button9.grid(row=1, column=2)
 button_div.grid(row=1, column=3)
 
-# Packing up the buttons for 0, clear, -, equals on Row3.
-button0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1)
+# Packing up the buttons for 0, clear, -, equals on Row4.
+button0.grid(row=4, column=1)
+button_clear.grid(row=4, column=0)
 button_sub.grid(row=4, column=2)
-button_equal.grid(row=4, column=3)
+button_equal.grid(row=4, column=3, rowspan=2)
 
+# Packing up the buttons for Power, Square, & Percentage on Row5. 
+button_pow.grid(row=5, column=0)
+button_sqr.grid(row=5, column=1)
+button_pst.grid(row=5, column=2)
+
+# Loop GUI. 
 root.mainloop()
